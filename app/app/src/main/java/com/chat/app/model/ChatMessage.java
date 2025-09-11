c64d3bcf7606f0c177383cfab0159d7900babc09
Nethra G S <nethra120304@gmail.com>
2025-09-12 01:26:57 +0530

    updated version with UI

---
+++ app/app/src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,40 @@
+package com.chat.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    private Long id;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private  String sender;
+    private  String content;
+    private MessageType type;
+}
